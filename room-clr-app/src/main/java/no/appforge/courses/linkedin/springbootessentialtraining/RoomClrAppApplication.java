e75c44e49411e0617cc64823a4a74e6efb39977c
Rolf Pedersen <rolfhsp@gmail.com>
2018-05-14 20:12:15 +0200

    3. Command line application. Add new module with Spring Initializer, no further dependencies.

---
+++ room-clr-app/src/main/java/no/appforge/courses/linkedin/springbootessentialtraining/RoomClrAppApplication.java
@@ -0,0 +1,12 @@
+package no.appforge.courses.linkedin.springbootessentialtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomClrAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomClrAppApplication.class, args);
+	}
+}
