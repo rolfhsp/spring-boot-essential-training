f11e6feb71866d100caf3d4004fa119a675fe05c
Rolf Pedersen <rolfhsp@gmail.com>
2018-05-14 22:58:03 +0200

    3. Add toString() to Room class.

--- room-clr-app/src/main/java/no/appforge/courses/linkedin/springbootessentialtraining/Room.java
+++ room-clr-app/src/main/java/no/appforge/courses/linkedin/springbootessentialtraining/Room.java
@@ -24,6 +24,17 @@ public class Room {
         this.id = id;
     }
 
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Room{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", number='").append(number).append('\'');
+        sb.append(", info='").append(info).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
     public String getName() {
         return name;
     }
